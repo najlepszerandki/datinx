5be5cd89ec137f35159dec07d8523e365a83dfe1
najlepszerandki <60196225+najlepszerandki@users.noreply.github.com>
2020-01-22 23:53:43 +0100

    first commit

---
+++ js/steps.js
@@ -0,0 +1,3 @@
+$(function(){$('.btn:eq(0), .btn:eq(1)').on('click',function(){$('#q1').hide();$('#q2').show();})
+$('.btn:eq(2), .btn:eq(3)').on('click',function(){$('#q2').hide();$('#q3').show();})
+$('.btn:eq(4), .btn:eq(5)').on('click',function(){$('#q3').hide();$('#q4').show();})})
